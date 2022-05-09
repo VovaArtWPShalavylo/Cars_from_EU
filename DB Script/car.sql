4cf123cda17849dd51fe197b17fd19d386416308
Vitalii <105131308+Ancorie@users.noreply.github.com>
2022-05-09 20:09:42 +0300

    Create car.sql
    Add script for creation car table

---
+++ DB Script/car.sql
@@ -0,0 +1,16 @@
+create table cars.car(
+  car_id int primary key,
+  brand varchar(50),
+  model varchar(50),
+  [year] varchar(10),
+  body varchar (30),
+  transmission varchar(30),
+  fuel_type varchar(30),
+  engine_capacity varchar(20),
+  mileage varchar(25),
+  color varchar (25),
+  weight varchar(20),
+  doors varchar(20),
+  wheel_size varchar(20),
+  vin_code varchar(40)
+  )
