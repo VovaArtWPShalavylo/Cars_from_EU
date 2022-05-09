432aa8267c0ff3c38edd34e7a18db0a6bbed7c75
Vitalii <105131308+Ancorie@users.noreply.github.com>
2022-05-09 20:50:11 +0300

    Delete cars_delivery.sql

--- DB Script/cars_delivery.sql
+++
@@ -1,9 +0,0 @@
-create table cars.delivery(
-	delivery_id int Primary key,
-	location_id_from int,
-	location_id_to int,
-	method varchar(30),
-	price varchar(20),
-	insurance varchar(10),
-	arrival_time varchar(20)
-	)
